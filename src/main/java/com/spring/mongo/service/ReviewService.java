edbd6525a3af3af22068e89aaf3c1f1a64715ead
elenaklimovich <eklimovich@systematica-consulting.ru>
2023-05-24 18:32:10 +0300

    mongoTemplate added to update movie with review

--- src/main/java/com/spring/mongo/service/ReviewService.java
+++ src/main/java/com/spring/mongo/service/ReviewService.java
@@ -1,8 +1,12 @@
 package com.spring.mongo.service;
 
+import com.spring.mongo.model.Movie;
 import com.spring.mongo.model.Review;
 import com.spring.mongo.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,8 +17,18 @@ public class ReviewService {
     @Autowired
     private ReviewRepository repository;
 
-    public Review addReview(String body) {
+    @Autowired
+    private MongoTemplate template;
+
+    public Review addReview(String body, String imdbId) {
         Review review = new Review(body, LocalDateTime.now(), LocalDateTime.now());
-        return repository.insert(review);
+        repository.insert(review);
+
+        template.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviews").value(review))
+                .first();
+
+        return review;
     }
 }
